95d69b8a72c9688435a559b77bbba2b28b943e55
oleg-romanov <55031494+oleg-romanov@users.noreply.github.com>
2021-05-29 01:06:03 +0300

    Добавил логотип приложения

--- Toast/Services/URLPath.swift
+++ Toast/Services/URLPath.swift
@@ -12,5 +12,5 @@ enum URLPath: String {
     case ip1 = "http://192.168.1.6:8080"
     case ip2 = "http://192.168.1.83:8080"
 
-    static let path = URLPath.ip2.rawValue
+    static let path = URLPath.ip1.rawValue
 }
